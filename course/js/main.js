2f0d30a5d08d55f820cc3c906a7d68ff2acb4d8d
Roger Rezende <javielrezende@gmail.com>
2018-03-14 15:08:31 -0300

    Herança com js, extends

--- course/js/main.js
+++ course/js/main.js
@@ -1,3 +1,15 @@
+"use strict";
+
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 // ----------Aula ARROWS FUNCTION----------------------------------------------------------------------
 // ----------PRIMEIRO EXEMPLO SEM ES6----------------------------------------------------------------------
 /*
@@ -137,9 +149,50 @@ console.log(i);
 
 */
 
-// ----------Aula VAR OF----------------------------------------------------------------------
+// ----------Aula CLASSES----------------------------------------------------------------------
 //-----------ANTES DO ES 6 ERA FEITO ASSIM-----------------------------------------------------------
+var People = function () {
+    function People(name, email, phone) {
+        _classCallCheck(this, People);
 
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 
+    _createClass(People, [{
+        key: "toString",
+        value: function toString() {
+            return "Name: " + this.name + "\n        Email: " + this.email + "\n        Phone: " + this.phone + "\n        ";
+        }
+    }]);
+
+    return People;
+}();
+
+var Client = function (_People) {
+    _inherits(Client, _People);
+
+    function Client(id, name, email, phone) {
+        _classCallCheck(this, Client);
+
+        var _this = _possibleConstructorReturn(this, (Client.__proto__ || Object.getPrototypeOf(Client)).call(this, name, email, phone));
+
+        _this.id = id;
+        return _this;
+    }
+
+    _createClass(Client, [{
+        key: "toString",
+        value: function toString() {
+            return "id: " + this.id + "\n    " + _get(Client.prototype.__proto__ || Object.getPrototypeOf(Client.prototype), "toString", this).call(this) + "\n    ";
+        }
+    }]);
+
+    return Client;
+}(People);
+
+var test = new Client(1, "Guikherme", "guilherme@email.com", "2234442");
+
+console.log(test.toString());
 //--------------------------AGORA COM ES 6------------------------------------------------------
-"use strict";
--- course/src/app.js
+++ course/src/app.js
@@ -196,9 +196,36 @@ console.log(i);
 
 
 
-// ----------Aula VAR OF----------------------------------------------------------------------
+// ----------Aula CLASSES----------------------------------------------------------------------
 //-----------ANTES DO ES 6 ERA FEITO ASSIM-----------------------------------------------------------
+class People{
+    constructor(name, email, phone){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 
+    toString(){
+        return `Name: ${this.name}
+        Email: ${this.email}
+        Phone: ${this.phone}
+        `;
+}
+}
 
+class Client extends People{
+constructor(id, name, email, phone){
+    super(name, email, phone);
+    this.id = id;
+    }
+    toString(){
+    return`id: ${this.id}
+    ${super.toString()}
+    `
+    }
+}
+var test = new Client(1, "Guikherme", "guilherme@email.com", "2234442");
+
+console.log(test.toString());
 //--------------------------AGORA COM ES 6------------------------------------------------------
 
